f201cac6b61bfc89ba75ff7b20612016a605fc2c
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-03-15 15:00:13 +0300

    improve web manifest

---
+++ config/manifest.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: "octane-notes",
+    short_name: "octane-notes",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons:[{"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
