e25e53011b53c2eb96e3c8de4db5553665e870fe
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-03-15 15:10:42 +0300

    fix icon paths

--- config/manifest.js
+++ config/manifest.js
@@ -7,12 +7,12 @@ module.exports = function(/* environment, appConfig */) {
   return {
     name: "octane-notes",
     short_name: "octane-notes",
-    description: "",
+    description: "notes - an simple Ember Octane apllication",
     start_url: "/demo-notes/",
     display: "standalone",
     background_color: "#fff",
     theme_color: "#fff",
-    icons:[{"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}],
+    icons:[{"src":"android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"android-chrome-512x512.png","sizes":"512x512","type":"image/png"}],
     ms: {
       tileColor: '#fff'
     }
